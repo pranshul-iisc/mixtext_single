16e2f649f90234c1f732421b7fc2b57444177279
Prd4tr <pranshu.kushwaha@gmail.com>
2021-01-22 12:29:54 +0530

    adding

--- code/train.py
+++ code/train.py
@@ -585,7 +585,8 @@ def train(labeled_trainloader, unlabeled_trainloader, model, optimizer, schedule
             outputs_u2 = model(inputs_u2)
             outputs_ori = model(inputs_ori)
             print("output:",type(outputs_u), outputs_u.shape, outputs_u)
-            outputs = torch.FloatTensor(out_u)
+            outputs = [[(1 if j ==i else 0) for j in range(10)] for i in outputs_u]
+            outputs = torch.FloatTensor(outputs)
             print("output2:", type(outputs), outputs.shape, outputs)
             # Based on translation qualities, choose different weights here.
             # For AG News: German: 1, Russian: 0, ori: 1
